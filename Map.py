2eaabc5a679bb38ff00b49ecbb45fc4cf5c63359
Sulav Paudel <110334339+itissulav@users.noreply.github.com>
2022-08-06 01:07:15 +0545

    Update Map.py

--- Map.py
+++ Map.py
@@ -16,12 +16,12 @@ with open('Database.csv') as file:
         fill = True,
         fill_color = "blue",
         fill_opacity = 0.1 ).add_to(map)
+map.save("map.html")
 
-        folium.Polygon([(27.70605851,85.30595914), (27.67490210891375, 85.30299177386615),(28.70605851,85.30595914)], color="blue",
-               weight=2,
-               fill=True,
-               fill_color="orange",
-               fill_opacity=0.4).add_to(map)
+''' folium.Polygon([(27.70605851,85.30595914), (27.67490210891375, 85.30299177386615),(28.70605851,85.30595914)], color="blue",
+            weight=2,
+            fill=True,
+            fill_color="orange",
+            fill_opacity=0.4).add_to(map)'''
         
-        map.save("map.html")
 
